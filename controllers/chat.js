edf0145d2beaa54571b1ab25f47108c2e3ccb7d3
Joe Johnson <joe@Joes-MacBook-Pro.local>
2018-01-09 16:14:43 -0800

    Server-side socket up, client struggling with routes... to do.

--- controllers/chat.js
+++ controllers/chat.js
@@ -2,11 +2,24 @@ var express = require('express');
 var passport = require('../config/passportConfig');
 var db = require('../models');
 var router = express.Router();
+var http = require('http').Server(router);
+var io = require('socket.io')(http);
+
+router.use(express.static('public'))
 
 router.get('/', function(req, res){
-  console.log('Hello Chat!')
-  res.send('home');
+  res.sendFile('/index.html');
 });
 
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+// http.listen(4000, function(){
+//   console.log('listening on *:4000');
+// });
+http.listen();
 
 module.exports = router;
