eb20e5eee721af5429281a7b63bf24e126b4ab02
Joe Johnson <joe@Joes-MacBook-Pro.local>
2018-01-10 16:09:58 -0800

    Test success on client data received. Socket connection established. Next populate chat-room(s).

--- public/script.js
+++ public/script.js
@@ -5,11 +5,23 @@ var socket;
 function setup() {
   socket = io.connect('http://localhost:3000');
 
-  // socket.on('mouse', function(data) {// When we receive data
-  //   console.log("Got: " + data.x + " " + data.y);
-  //   // Draw a blue circle
-  //   fill(0,0,255);
-  //   noStroke();
-  //   ellipse(data.x, data.y, 20, 20);
-  // });
+  socket.on('mouse', function(data) {// When we receive data
+    console.log("Got: " + data.x + " " + data.y);
+  });
 }
+
+function mouseDragged() {
+  sendmouse(mouseX,mouseY);
+}
+
+function sendmouse(xpos, ypos) {
+  // We are sending!
+  console.log("sendmouse: " + xpos + " " + ypos);
+  // Make a little object with  and y
+  var data = {
+    x: xpos,
+    y: ypos
+  };
+  // Send that object to the socket
+  socket.emit('mouse',data);
+  }
