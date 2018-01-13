489a955214966cc59e7bfb6d6eaf18f9ea755742
Joe Johnson <joe@Joes-MacBook-Pro.local>
2018-01-13 11:25:04 -0800

    CSS Styling, added Location img to backgroung per API call.

---
+++ public/backImg.js
@@ -0,0 +1,20 @@
+function geoFindImg() {
+  var output = document.getElementById("out");
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(latitude, longitude);
+    var img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=600x600&sensor=false";
+    output.appendChild(img);
+  }
+
+  function error() {
+    console.log('error geolocal rendering');
+  }
+
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
