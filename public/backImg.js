1c4aebed27fee0c72e3b3d95b2f2ce34640245e9
Joe Johnson <joe@Joes-MacBook-Pro.local>
2018-01-13 13:41:02 -0800

    CSS Styling, auto scroll.

--- public/backImg.js
+++ public/backImg.js
@@ -1,6 +1,5 @@
 function geoFindImg() {
   var output = document.getElementById("out");
-
   function success(position) {
     var latitude  = position.coords.latitude;
     var longitude = position.coords.longitude;
@@ -10,11 +9,8 @@ function geoFindImg() {
     img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=600x600&sensor=false";
     output.appendChild(img);
   }
-
   function error() {
     console.log('error geolocal rendering');
   }
-
-
   navigator.geolocation.getCurrentPosition(success, error);
 }
